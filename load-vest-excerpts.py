b8d536e5801e4333706758a8ab0be538db2b7a1a
Michal Migurski <mike@teczno.com>
2021-10-07 21:08:42 -0700

    Prepared LA/GA load_votes() tests

---
+++ load-vest-excerpts.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import geopandas
+
+assemble = __import__('assemble-state')
+
+if __name__ == '__main__':
+    df1a = geopandas.read_file('test/ga_2016.gpkg')
+    print(df1a.sum())
+    
+    df1b = assemble.load_votes('test/ga_2016.gpkg')
+    assert df1b['US President 2016 - REP'].sum() == 2660
+    assert df1b['US President 2016 - DEM'].sum() == 4265
+    assert df1b['US Senate 2016 - REP'].sum() == 3276
+    assert df1b['US Senate 2016 - DEM'].sum() == 3433
