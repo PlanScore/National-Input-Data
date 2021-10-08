097f60f679303da5218225534ad397113c0e41ac
Michal Migurski <mike@teczno.com>
2021-10-07 22:28:35 -0700

    Added test for LA 2020 senate race

--- load-vest-excerpts.py
+++ load-vest-excerpts.py
@@ -5,8 +5,6 @@ assemble = __import__('assemble-state')
 
 if __name__ == '__main__':
     df1a = geopandas.read_file('test/ga_2016.gpkg')
-    print(df1a.sum())
-    
     df1b = assemble.load_votes('test/ga_2016.gpkg')
     assert df1b['US President 2016 - REP'].sum() == 2660
     assert df1b['US President 2016 - DEM'].sum() == 4265
@@ -16,8 +14,6 @@ if __name__ == '__main__':
     assert df1b['US Senate 2016 - Other'].sum() == 221
 
     df2a = geopandas.read_file('test/ga_2020.gpkg')
-    print(df2a.sum())
-    
     df2b = assemble.load_votes('test/ga_2020.gpkg')
     assert df2b['US President 2020 - REP'].sum() == 17359
     assert df2b['US President 2020 - DEM'].sum() == 5468
@@ -27,16 +23,12 @@ if __name__ == '__main__':
     assert df2b['US Senate 2020 - Other'].sum() == 0
 
     df3a = geopandas.read_file('test/ca_2020.gpkg')
-    print(df3a.sum())
-    
     df3b = assemble.load_votes('test/ca_2020.gpkg')
     assert df3b['US President 2020 - REP'].sum() == 496
     assert df3b['US President 2020 - DEM'].sum() == 4869
     assert df3b['US President 2020 - Other'].sum() == 45 + 42 + 9 + 29
 
     df4a = geopandas.read_file('test/la_2016.gpkg')
-    print(df4a.sum())
-    
     df4b = assemble.load_votes('test/la_2016.gpkg')
     assert df4b['US President 2016 - REP'].sum() == 4003
     assert df4b['US President 2016 - DEM'].sum() == 492
@@ -44,3 +36,12 @@ if __name__ == '__main__':
     assert df4b['US Senate 2016 - DEM'].sum() == 303
     assert df4b['US President 2016 - Other'].sum() == 46 + 17 + 11 + 11 + 13
     assert df4b['US Senate 2016 - Other'].sum() == 0
+
+    df5a = geopandas.read_file('test/la_2020.gpkg')
+    df5b = assemble.load_votes('test/la_2020.gpkg')
+    assert df5b['US President 2020 - REP'].sum() == 5936
+    assert df5b['US President 2020 - DEM'].sum() == 1453
+    assert df5b['US Senate 2020 - REP'].sum() == 5654 + 143
+    assert df5b['US Senate 2020 - DEM'].sum() == 406 + 84 + 587 + 90 + 16
+    assert df5b['US President 2020 - Other'].sum() == 64 + 17 + 36
+    assert df5b['US Senate 2020 - Other'].sum() == 19 + 33 + 68 + 84 + 20 + 14 + 22 + 23
